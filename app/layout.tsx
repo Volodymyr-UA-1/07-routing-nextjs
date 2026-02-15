21f6036845e6f9c4b5ea0374b9f9f267d726380d
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2026-02-15 18:48:48 +0200

    add module.css + not-found.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import './global.css';
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
